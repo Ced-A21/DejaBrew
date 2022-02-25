cd9c622a728660b4caf7cf2ccf900082ae450edc
jshalstr <joshaleisterv@gmail.com>
2022-02-25 18:41:16 +0800

    modified: quantity and delete functionality added

--- Cart.aspx.cs
+++ Cart.aspx.cs
@@ -12,10 +12,34 @@ namespace DejaBrew
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                CartActions shoppingCart = new CartActions();
+                GridCart.DataSource = shoppingCart.GetCartItems();
+                GridCart.DataBind();
+            }
+        }
+
+        protected void GridCart_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
             CartActions shoppingCart = new CartActions();
+            int cartItemID = Convert.ToInt32(GridCart.DataKeys[e.RowIndex].Values[0]);
+            shoppingCart.RemoveCartItem(cartItemID);
             GridCart.DataSource = shoppingCart.GetCartItems();
             GridCart.DataBind();
         }
 
+        protected void qty_TextChanged(object sender, EventArgs e)
+        {
+            CartActions shoppingCart = new CartActions();
+            TextBox TBqty = sender as TextBox;
+            GridViewRow gvr = TBqty.NamingContainer as GridViewRow;
+            int cartItemID = Convert.ToInt32(GridCart.DataKeys[gvr.RowIndex].Values[0]);
+            decimal price = Convert.ToDecimal(GridCart.DataKeys[gvr.RowIndex].Values[1]);
+            int qty = Convert.ToInt32(TBqty.Text);
+            shoppingCart.ChangeQuantity(cartItemID, qty, price);
+            GridCart.DataSource = shoppingCart.GetCartItems();
+            GridCart.DataBind();
+        }
     }
 }
--- CartActions.cs
+++ CartActions.cs
@@ -9,20 +9,20 @@ namespace DejaBrew
     public class CartActions
     {
         public int cartId { get; set; }
-        public void AddToCart(int id)
+        public void AddToCart(int productID)
         {
             cartId = 1; // Test, replace once cart is tied to an account
 
             SqlConnection conn = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\DejaBrew.mdf;Integrated Security=True;MultipleActiveResultSets=True;Application Name=EntityFramework");
             conn.Open();
             SqlCommand getCartItem = new SqlCommand("SELECT Id, ProductID, CartID, ItemQty, ItemPrice FROM CartItems WHERE CartID = 1 AND ProductID = @ParamProductID", conn); // Test, use cart table when card is tied to an account
-            getCartItem.Parameters.AddWithValue("@ParamProductID", id);
+            getCartItem.Parameters.AddWithValue("@ParamProductID", productID);
             var cartItem = getCartItem.ExecuteReader();
             cartItem.Read();
 
             //Find price per product
             SqlCommand findPrice = new SqlCommand("SELECT ProductPrice FROM Products WHERE Id = @ParamProdID", conn);
-            findPrice.Parameters.AddWithValue("@ParamProdID", id);
+            findPrice.Parameters.AddWithValue("@ParamProdID", productID);
             decimal? price = findPrice.ExecuteScalar() as decimal?;
 
             if (!cartItem.HasRows)
@@ -30,7 +30,7 @@ namespace DejaBrew
                 // Then add cart item
                 SqlCommand addCartItem = new SqlCommand("INSERT INTO CartItems(ProductID, CartID, ItemQty, ItemPrice)" +
                     "VALUES(@ParamProductId, @ParamCartID, @ParamItemQty, @ParamItemPrice)", conn);
-                addCartItem.Parameters.AddWithValue("@ParamProductID", id);
+                addCartItem.Parameters.AddWithValue("@ParamProductID", productID);
                 addCartItem.Parameters.AddWithValue("@ParamCartID", cartId);
                 addCartItem.Parameters.AddWithValue("@ParamItemQty", 1);
                 addCartItem.Parameters.AddWithValue("@ParamItemPrice", price);
@@ -46,6 +46,28 @@ namespace DejaBrew
             }
             conn.Close();
         }
+
+        public void RemoveCartItem(int cartItemID)
+        {
+            SqlConnection conn = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\DejaBrew.mdf;Integrated Security=True;MultipleActiveResultSets=True;Application Name=EntityFramework");
+            conn.Open();
+            SqlCommand removeCartItem = new SqlCommand("DELETE FROM CartItems WHERE Id = @ParamCartItemID", conn);
+            removeCartItem.Parameters.AddWithValue("@ParamCartItemID", cartItemID);
+            removeCartItem.ExecuteNonQuery();
+            conn.Close();
+        }
+
+        public void ChangeQuantity(int cartItemID, int qty, decimal price)
+        {
+            SqlConnection conn = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\DejaBrew.mdf;Integrated Security=True;MultipleActiveResultSets=True;Application Name=EntityFramework");
+            conn.Open();
+            SqlCommand updateQty = new SqlCommand("UPDATE CartItems SET ItemQty = @ParamSetQty WHERE Id = @ParamId; UPDATE CartItems SET ItemPrice = @ParamPrice * ItemQty WHERE Id = @ParamId", conn);
+            updateQty.Parameters.AddWithValue("@ParamSetQty", qty);
+            updateQty.Parameters.AddWithValue("@ParamId", cartItemID);
+            updateQty.Parameters.AddWithValue("@ParamPrice", price);
+            updateQty.ExecuteNonQuery();
+            conn.Close();
+        }
         
         public SqlDataReader GetCartItems()
         {
@@ -53,7 +75,7 @@ namespace DejaBrew
 
             SqlConnection conn = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\DejaBrew.mdf;Integrated Security=True;MultipleActiveResultSets=True;Application Name=EntityFramework");
             conn.Open();
-            SqlCommand getCartItem = new SqlCommand("SELECT Products.ProductName, ItemQty, ItemPrice FROM CartItems INNER JOIN Products ON CartItems.ProductID = Products.Id WHERE CartID = 1", conn); // Test, use cart table when card is tied to an account
+            SqlCommand getCartItem = new SqlCommand("SELECT CartItems.Id, Products.ProductPrice, Products.ProductName, ItemQty, ItemPrice FROM CartItems INNER JOIN Products ON CartItems.ProductID = Products.Id WHERE CartID = 1", conn); // Test, use cart table when card is tied to an account
             var cartItems = getCartItem.ExecuteReader();
             return cartItems;
         }
